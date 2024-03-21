7ba7de4120d870ed70c460fb282b5b5365abc502
david <kaneshiuchiha@gmail.com>
2024-03-21 14:27:45 -0500

    footer added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,12 +4,13 @@ import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <main className='mt-54 text-center w-full p-4 bg-neutral-300 text-white pb-20'>
-            Derechos reservados 
-            <div>
-                genero.derechos@uce.edu.ec
+        <footer class="bg-red-900 text-white justify-center items-center shadow  ">
+            <div class="w-full mx-auto max-w-screen-xl p-4 items-center flex justify-center">
+                <span class="text-sm text-center  sm:text-center ">© 2024 <a href="https://flowbite.com/" class="hover:underline">UNIGED</a>. All Rights Reserved.
+                </span>
             </div>
-        </main>
+        </footer>
+
     )
 }
 
